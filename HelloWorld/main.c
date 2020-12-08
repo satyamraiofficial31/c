6af3433d12fa79c44eca4c1ad8a4ac08dd209b32
satyamrai31 <satyamraiofficial31@gmail.com>
2020-12-08 18:33:39 +0530

    Prints hello world 10 times

--- HelloWorld/main.c
+++ HelloWorld/main.c
@@ -3,10 +3,11 @@
 /*
  * @author jelathro
  * @date 2/18/13
- * 
+ *
  * Print "Hello, World!" to the console
  */
-int main(void){
+int main(){
+    for(int i=0;i<10;i++)
     printf("Hello, World!\n");
     return 0;
 }
